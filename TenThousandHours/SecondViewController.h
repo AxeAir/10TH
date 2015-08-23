38fc31b3f6ec9084a100ac97a252539661e60d97
HarriesChen <info@mrchenhao.com>
2015-08-23 22:58:30 +0800

    project init

---
+++ TenThousandHours/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  TenThousandHours
+//
+//  Created by ChenHao on 8/23/15.
+//  Copyright (c) 2015 AxeAir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
